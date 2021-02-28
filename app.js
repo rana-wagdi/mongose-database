cf421d18873133625d273f2ff91fc73e612e01ec
rana2020701 <ranawagdi4@gmail.com>
2021-02-28 07:32:10 +0200

    use forEach

--- app.js
+++ app.js
@@ -75,16 +75,9 @@ Fruit.find(function(err, fruits){
         console.log(err);
     }else {
         console.log(fruits);
+
+        fruits.forEach(function(fruit){
+            console.log(fruit.name)
+        })
     }
 })
-const findDocuments = function(db, callback) {
-    //Get the documents collection
-    const collection = db.collection('fruits');
-    //Find some documents
-    collection.find({}).toArray(function(err, fruits){
-        assert.equal(err, null);
-        console.log("Found the following records");
-        console.log(fruits)
-        callback(fruits)
-    })
-}
